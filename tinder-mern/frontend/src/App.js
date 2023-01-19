e2ed31815e50187e942e2fd7e3de0f88e20531a4
Bala Chandar <117364951+Balachandar1997@users.noreply.github.com>
2023-01-19 14:02:54 +0530

    commit

---
+++ tinder-mern/frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/Header';
+import Tindercard from './components/Tindercard';
+
+function App() {
+  return (
+    <div className="App">
+   <Header />
+   <Tindercard />
+    </div>
+  );
+}
+
+export default App;
